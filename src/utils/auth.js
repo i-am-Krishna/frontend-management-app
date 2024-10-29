1cbaa0cd93c66af921d3d6dab17f061fe0d6e147
i-am-Krishna <kumarkrishn663@gmail.com>
2024-10-29 18:24:46 +0530

    update authentication system

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,18 +1,15 @@
 import axios from 'axios';
 
 export const isAuthenticated = async () => {
-  try {
-    const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/v1/user/checkauth`, { withCredentials: true });
-    console.log(6)
-    return response.status === 200;
-  } catch (error) {
-    await new Promise(resolve => setTimeout(resolve, 500));
     try {
-      const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/v1/user/checkauth`, { withCredentials: true });
-      console.log(12)
-      return response.status === 200;
-    } catch {
-      return false;
+      const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/v1/user/checkauth`,{ withCredentials: true });
+      if (response) {
+        return true 
+      }
+      return true 
+    } catch (error) {
+      return false
     }
   }
-};
+  
+  
