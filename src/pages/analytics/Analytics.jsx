ca6c4a112cf9215189e7cdc215fd426f4f4568a6
i-am-Krishna <kumarkrishn663@gmail.com>
2024-11-20 01:16:16 +0530

    updating analytics loading component

--- src/pages/analytics/Analytics.jsx
+++ src/pages/analytics/Analytics.jsx
@@ -2,14 +2,18 @@ import React, { useEffect, useState } from 'react'
 import styles from './Analytics.module.css'
 import Circle from '../../components/circle/Circle';
 import axios from 'axios';
+import Skeleton from 'react-loading-skeleton';
+import "react-loading-skeleton/dist/skeleton.css";
 
 const Analytics = () => {
     const [data,setData] = useState({});
+    const [loading,setLoading] = useState(true);
 
     useEffect(()=>{
       const getAnalytics = async()=>{
         const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/v1/task/count`,{ withCredentials: true });
         setData(response.data.taskCounts);
+        setLoading(false);
       }
       getAnalytics();
     },[])
@@ -21,16 +25,31 @@ const Analytics = () => {
       
       <div className={styles.box}>
         <div className={styles.firstSec}>
+          {loading ? (<Skeleton height={40} width={"100%"} count={4}/>) 
+          :
+          (
+            <>
             <div><p className={styles.para}><Circle height='12px' width='12px' color='#90C4CC'/>Backlog Tasks</p> <p>{data.status?.backlog}</p></div>
             <div><p className={styles.para}><Circle height='12px' width='12px' color='#90C4CC'/>To-do Tasks</p> <p>{data.status?.todo}</p></div>
             <div><p className={styles.para}><Circle height='12px' width='12px' color='#90C4CC'/>In-Progress Tasks</p> <p>{data.status?.inprogress}</p></div>
             <div><p className={styles.para}><Circle height='12px' width='12px' color='#90C4CC'/>Completed Tasks</p> <p>{data.status?.done}</p></div>
+          </>
+          )
+          }
         </div>
         <div className={styles.secondSec}>
+        {loading ? (<Skeleton height={40} width={"100%"} count={4}/>) 
+          :
+          (
+            <>
+          
             <div><p className={styles.para}> <Circle height='12px' width='12px' color='#90C4CC'/>Low Priority</p> <p>{data.priority?.lowpriority}</p></div>
             <div><p className={styles.para}> <Circle height='12px' width='12px' color='#90C4CC'/>Moderate Priority</p> <p>{data.priority?.moderatepriority}</p></div>
             <div><p className={styles.para}> <Circle height='12px' width='12px' color='#90C4CC'/>High Priority</p> <p>{data.priority?.highpriority}</p></div>
             <div><p className={styles.para}> <Circle height='12px' width='12px' color='#90C4CC'/>Due Date Tasks</p> <p>{data.dueDateCount}</p></div>
+            </>
+          )
+          }
         </div>
       </div>
     </div>
